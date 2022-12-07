ea2f6be6693e1a7cb79c32ffea200123a5f77ed3
Mingzhe Zhang <mingzhe.zhang@uqconnect.edu.au>
2022-12-07 19:00:17 +1000

    Update decisionstump.py

--- src/decisionstump.py
+++ src/decisionstump.py
@@ -1,53 +1,69 @@
-import datetime
 from time import time
-
+import datetime
 import numpy as np
-
+import warnings
 
 NUM_STEPS = 10
-EPS = 1e-10
+EPS = np.finfo(float).eps
 
 
 class DecisionStump:
-    def __init__(self, 
-                 prior, 
-                 weight, 
-                 HX_u, 
-                 HX_p, 
-                 unlabeled_losses, 
-                 random_selection, 
-                 nnpu, 
-                 beta, 
-                 id):
+    """An Decision Stump as the weak learner of Ada-PU.
+    
+    This class implements the Decision Stump follow [1].
+    
+    .. versionadded:: 1.0.1
+    
+    Parameters
+    ----------
+    prior : float
+        prior probability of positive data
+    weight : dict
+        samples' weight
+    random_selection : int
+        whether to use the random selection strategy
+    ada_prediction_p : np.array
+        positive data predictions (HX_positive)
+    ada_prediction_u : np.array
+        unlabeled data predictions (HX_unlabeled)
+    beta:
+        control parameter
+        
+    References
+    ----------
+    .. [1] Harrington P. Machine learning in action. Simon and Schuster, 2012.
+    """
+    def __init__(self, prior, weight, ada_predictions_u, ada_predictions_p, random_selection, beta):
 
         self.beta = beta
-        self.id = id
+        self.random_selection = random_selection
+        self.prior = prior
+        self.weight = weight
+        
+        self.ada_predictions_u = ada_predictions_u
+        self.ada_predictions_p = ada_predictions_p
         
         self.inequal = 'lt'
         self.feature_idx = 0.0
         self.threshold = 0.0
-        self.alpha = 1.0
-        self.run_time = 0.0
-        
-        self.random_selection = random_selection
-        self.nnpu = nnpu
-
-        self.prior = prior
-
+        self.estimator_weight = 1.0
         self.min_error = float("inf")
         self.max_estimation = float("-inf")
 
-        self.miss = {}
-        self.weight = weight
+        self.best_prediction_p = None
+        self.best_prediction_u = None
+        
 
-        self.HX_u = HX_u
-        self.HX_p = HX_p
-        self.unlabeled_losses = unlabeled_losses
+    def get_target(self, num_p: int, num_u: int) -> np.array:
+        """Get the label of each sample.
 
-        self.best_p = None
-        self.best_u = None
+        Args:
+            num_p (_type_): number of positive samples
+            num_u (_type_): number of unlabeled samples
 
-    def get_target(self, num_p, num_u):
+        Returns:
+            np.array: labels of data
+        """
         y_train_p = np.ones(num_p)
         y_train_n = np.ones(num_p)
         y_train_u = np.ones(num_u)
@@ -55,26 +71,41 @@ class DecisionStump:
         y_train_u[y_train_u > 0] = -1
 
         return y_train_p, y_train_n, y_train_u
-    
-    
-    def create_seed(self):
-        current_time = datetime.datetime.now()
-        current_time = current_time.microsecond
-        
-        np.random.seed(current_time)
-        random_seed = np.random.uniform(low=0, high=1, size=None)
-        
-        return random_seed
+
+
+    def fitted_predict(self, X):
+        """Prediction function used after fitting progress.
+
+        Args:
+            X (np.array): training data  ``X``
+
+        Returns:
+            np.array: prediction results
+        """
+        x_column = X[:, int(self.feature_idx)]
+        predictions = np.ones(X.shape[0])
+
+        if self.inequal == 'lt':
+            predictions[x_column <= self.threshold] = -1
+        else:
+            predictions[x_column > self.threshold] = -1
+
+        return predictions
 
 
     def predict(self, x_train, feature_idx, inequal, threshold):
-        """
-        The predict function of decision stump.
+        """The predict function of decision stump.
 
-        :param x_train: training data
-        :return: The result of prediction (Numpy array)
+        Args:
+            x_train (np.array): training data 
+            feature_idx (int): feature index
+            inequal (str): ``<`` or ``>``, the side of decision tree, right/left
+            threshold (float): the threshold of each feature index
+            
+        Returns:
+            np.array: prediction results
         """
-        x_column = x_train[:, feature_idx]
+        x_column = x_train[:, int(feature_idx)]
         predictions = np.ones(x_train.shape[0])
 
         if inequal == 'lt':
@@ -83,124 +114,148 @@ class DecisionStump:
             predictions[x_column > threshold] = -1
 
         return predictions
+    
+
+    def _get_threshold_range(self, range_min, range_max, lower_bound, higher_bound, step):
+        """Get the feature's threshold
+
+        Args:
+            x_train_u (np.array): unlabeled training data
+            feature_index (int): feature index
+            step (int): step
+
+        Returns:
+            threshold (float): features' threshold
+        """
+            
+        if self.random_selection == 1:
+            time = datetime.datetime.now()
+            time = time.microsecond
+            np.random.seed(time)
+            rd = np.random.uniform(low=lower_bound, high=higher_bound, size=None)
+            threshold = rd * (range_max - range_min) + range_min
+        else:
+            lower_bound = lower_bound * (range_max - range_min) + range_min
+            higher_bound = higher_bound * (range_max - range_min) + range_min
+            step_size = (higher_bound - lower_bound) / NUM_STEPS
+            threshold = min(lower_bound + float(step) * step_size, higher_bound)
+
+        return threshold
 
 
     def build(self, x_train_p, x_train_u):
-        """
-        Processing of building stump
+        """The function of building a decision stump.
 
-        :param x_train_p: training positive data
-        :param x_train_u: training unlabeled data
-        :param y_train: label of unlabeled data
-        :return: None, update the parameter of decision stump
+        Args:
+            x_train_p (np.array): positive training data 
+            x_train_u (np.array): unlabeled training data 
         """
-        begin_time = time()
 
-        if len(x_train_p.shape) >= 3:
-            n_samples_p = x_train_p.shape[0]
-            n_samples_u = x_train_u.shape[0]
-            n_features = x_train_p.shape[1] * x_train_p.shape[2] * x_train_p.shape[3]
-        else:
-            n_samples_p = x_train_p.shape[0]
-            n_samples_u = x_train_u.shape[0]
-            n_features = x_train_p.shape[1]
+        n_samples_p = x_train_p.shape[0]
+        n_samples_u = x_train_u.shape[0]
+        n_features = x_train_p.shape[1]
 
         y_train_p, y_train_n, y_train_u = self.get_target(n_samples_p, n_samples_u)
 
-        self.best_p = y_train_p
-        self.best_u = y_train_u
+        self.best_prediction_p = y_train_p
+        self.best_prediction_u = y_train_u
 
         for feature_i in range(n_features):
-
             range_min = x_train_u[:, feature_i].min()
             range_max = np.max(x_train_u[:, feature_i])
-            step_size = (range_max - range_min) / NUM_STEPS
 
-            for i in range(NUM_STEPS + 1):
+            num_interval = len(np.unique(x_train_u[:, feature_i]))
+            
+            lower_bound = 0 - (1 / (num_interval-1))
+            higher_bound = 1 + (1 / (num_interval-1))
+            
+            for step in range(NUM_STEPS + 1):
+
                 for inequal in ['lt', 'gt']:
-
-                    if self.random_selection == 1:
-                        threshold = self.create_seed() * (range_max - range_min) + range_min
-                    else:
-                        threshold = min(range_min + float(i) * step_size, range_max)
-
+                    
+                    threshold = self._get_threshold_range(range_min, range_max, lower_bound, higher_bound, step)
+                    
                     curr_pred_p = self.predict(x_train_p, feature_i, inequal, threshold)
                     curr_pred_u = self.predict(x_train_u, feature_i, inequal, threshold)
 
-                    HX_u = self.HX_u
-                    HX_p = self.HX_p
+                    ada_predictions_u = self.ada_predictions_u
+                    ada_predictions_p = self.ada_predictions_p
 
                     # Calculate the global error
                     error, error_n, error_p = self.sum_error_calc(curr_pred_p, curr_pred_u)
 
-                    if self.nnpu == 1:  # nnpu
-                        if error_n < 0:
-                            continue
-                    else:               # upu
-                        if error < 0:
-                            continue
-
-                    if error >= 0.5:
+                    if error_n < 0 or error < 0 or error >= 0.5:
                         continue
+                    
+                    estimation = self.hx_target_function(n_samples_p, 
+                                                         n_samples_u, 
+                                                         ada_predictions_p, 
+                                                         ada_predictions_u, 
+                                                         curr_pred_p, 
+                                                         curr_pred_u)
 
-                    if len(self.unlabeled_losses) == 0:
-                        estimation = self.max_estimation
-                        if error < self.min_error:
-                            self.max_estimation = estimation
-                            self.min_error = error
+                    if estimation > self.max_estimation:
+                        self.max_estimation = estimation
+                        self.min_error = error
 
-                            self.inequal = inequal
-                            self.threshold = threshold
-                            self.feature_idx = feature_i
+                        self.inequal = inequal
+                        self.threshold = threshold
+                        self.feature_idx = feature_i
 
-                            self.best_p = curr_pred_p
-                            self.best_u = curr_pred_u
-                    else:
-                        estimation = self.target_function(self.prior, n_samples_p, n_samples_u, HX_p, HX_u, curr_pred_p, curr_pred_u)
-
-                        if estimation > self.max_estimation:
-                            self.max_estimation = estimation
-                            self.min_error = error
-
-                            self.inequal = inequal
-                            self.threshold = threshold
-                            self.feature_idx = feature_i
-
-                            self.best_p = curr_pred_p
-                            self.best_u = curr_pred_u
-
-        self.alpha = 0.5 * np.log((1.0 - self.min_error + EPS) / (self.min_error + EPS))
-
-        HX_u = HX_u * self.beta + np.multiply(self.alpha, self.best_u)
-        HX_p = HX_p * self.beta + np.multiply(self.alpha, self.best_p)
+                        self.best_prediction_p = curr_pred_p
+                        self.best_prediction_u = curr_pred_u
         
-        self.HX_u = HX_u
-        self.HX_p = HX_p
+        self.estimator_weight = 0.5 * np.log((1.0 - self.min_error + EPS) / (self.min_error + EPS))
+        self.estimator_weight *= self.beta
 
-        end_time = time()
-        self.run_time = end_time - begin_time
+        ada_predictions_u = ada_predictions_u + np.multiply(self.estimator_weight, self.best_prediction_u)
+        ada_predictions_p = ada_predictions_p + np.multiply(self.estimator_weight, self.best_prediction_p)
+
+        self.ada_predictions_u = ada_predictions_u
+        self.ada_predictions_p = ada_predictions_p
 
 
-    def target_function(self, 
-                        prior: float, 
-                        num_pos: int, 
-                        num_unlabeled: int, 
-                        y_preds_pos: np.array, 
-                        y_preds_unlabeled: np.array, 
-                        curr_pred_pos: np.array,
-                        curr_pred_unlabeled: np.array):
-        
-        term_pos_pos = (prior / num_pos) * np.sum(np.exp(-1 * y_preds_pos) * curr_pred_pos)
-        term_pos_neg = (prior / num_pos) * np.sum(np.exp(y_preds_pos) * curr_pred_pos)
+    def hx_target_function(self, num_pos, num_unlabeled, y_preds_pos, y_preds_unlabeled, curr_pred_pos,
+                        curr_pred_unlabeled):
+        """Calculating the target E_t. 
+
+        Args:
+            num_pos (int): positive samples' number
+            num_unlabeled (int): unlabeled samples' number
+            y_preds_pos (np.array): ada predict results of positive data (Hx)
+            y_preds_unlabeled (np.array): ada predict results of unlabeled data (Hx)
+            curr_pred_pos (np.array): current predict results of positive data
+            curr_pred_unlabeled (np.array): current predict results of unlabeled data
+
+        Returns:
+            E_t: the target value for selecting stump in each boost iteration
+        """
+
+        term_pos_pos = (self.prior / num_pos) * np.sum(np.exp(-1 * y_preds_pos) * curr_pred_pos)
+        term_pos_neg = (self.prior / num_pos) * np.sum(np.exp(y_preds_pos) * curr_pred_pos)
         term_unlabeled = (1 / num_unlabeled) * (np.sum(np.exp(y_preds_unlabeled) * curr_pred_unlabeled))
-     
+        
         return term_pos_pos + term_pos_neg - term_unlabeled
 
 
-    def sum_error_calc(self, curr_pred_p: np.array, curr_pred_u: np.array):
-        error_pn = self.prior * np.sum(self.weight['n'][curr_pred_p != -1])
+    def sum_error_calc(self, curr_pred_p, curr_pred_u):
+        """Calculating the error of each stump. 
+
+        Args:
+            curr_pred_p (np.array): current predict results of positive data
+            curr_pred_u (np.array): current predict results of unlabeled data
+
+        Returns:
+            current estimator's error
+        """
+        error_pn = np.sum(self.weight['n'][curr_pred_p != -1])
+        error_pn = self.prior * error_pn / np.sum(self.weight['n'])
+
         error_un = np.sum(self.weight['u'][curr_pred_u != -1])
-        error_pp = self.prior * np.sum(self.weight['p'][curr_pred_p != 1])
+        error_un = error_un / np.sum(self.weight['u'])
+
+        error_pp = np.sum(self.weight['p'][curr_pred_p != 1])
+        error_pp = self.prior * error_pp / np.sum(self.weight['p'])
 
         error = error_pp + error_un - error_pn
         error_n = error_un - error_pn
