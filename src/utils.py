9c27d6fb0024e85c5d617f4475551d11cf77e04a
Mingzhe Zhang <mingzhe.zhang@uqconnect.edu.au>
2022-12-07 19:01:57 +1000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,7 @@ def accuracy(output_p, output_n):
         accuracy = float(true_positive + true_negative) / float(all)
     return accuracy
 
+
 def accuracy_pn(y_pred, y_true):
     """
     Calculate the accuracy.
@@ -28,6 +29,7 @@ def accuracy_pn(y_pred, y_true):
     """
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
+
 
 def F1(output_p, output_n):
     true_positive = np.sum((np.sign(output_p) + 1) / 2)
@@ -44,12 +46,36 @@ def F1(output_p, output_n):
         F1 = 2 * precision * recall / (precision + recall)
     return F1, precision, recall
 
+
 def zero_one_training_loss(prior, y_pred_p, y_pred_n):
     cost_p = prior * np.mean((1 - np.sign(y_pred_p)) / 2)
     cost_n = np.mean((1 + np.sign(y_pred_n)) / 2)
     cost_p_ = prior * np.mean((1 + np.sign(y_pred_p)) / 2)
     cost = cost_p + cost_n - cost_p_
     return cost
+
+
+def nn_zero_one_training_loss(prior, y_pred_p, y_pred_n):
+    cost = np.mean((1 + np.sign(y_pred_n)) / 2) - prior * np.mean((1 + np.sign(y_pred_p)) / 2)
+    cost = max(cost, 0)
+    cost = cost + prior * np.mean((1 - np.sign(y_pred_p)) / 2)
+    return cost
+
+
+def exp_training_loss(prior, y_pred_p, y_pred_n):
+    cost_p = prior * np.mean(np.exp(-y_pred_p))
+    cost_n = np.mean(np.exp(y_pred_n))
+    cost_p_ = prior * np.mean(np.exp(y_pred_p))
+    cost = cost_p + cost_n - cost_p_
+    return cost
+
+
+def nn_exp_training_loss(prior, y_pred_p, y_pred_n):
+    cost = np.mean(np.exp(y_pred_n)) - prior * np.mean(np.exp(y_pred_p))
+    cost = max(cost, 0)
+    cost = cost + prior * np.mean(np.exp(-y_pred_p))
+    return cost
+
 
 def zero_one_test(output_p, output_n, prior):
     cost = prior * np.mean((1 - np.sign(output_p)) / 2)
